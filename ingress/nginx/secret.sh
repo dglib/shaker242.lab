06425dc6ab0b6e922da3a786acd3bececa78be88
Shaker Gilbert <shaker242@gmail.com>
2020-02-22 15:38:45 -0500

    update

--- ingress/nginx/secret.sh
+++ ingress/nginx/secret.sh
@@ -3,7 +3,7 @@ apiVersion: v1
 kind: Secret 
 metadata:
   name: default-server-secret 
-  namespace: nginx-ingress
+  namespace: ingress-nginx
 data:
   tls.crt: $(sudo -i cat /Users/shaker/certs/_.apps.shaker242.lab.crt | base64 | tr -d '\n')
   tls.key: $(sudo -i cat /Users/shaker/certs/_.apps.shaker242.lab.key | base64 | tr -d '\n')
